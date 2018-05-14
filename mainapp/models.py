2cedd7a26e91347be964402be124c8ad50f0a458
lyutenkova <annalyutenkova@icloud.com>
2018-05-14 13:06:16 +0300

    add database

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from mainapp.mixins import CreateMixin
+from datetime import datetime
 
-class ComparativeApproach(CreatedMixin):
+class ComparativeApproach(CreateMixin):
     mark = models.CharField(max_length=256, blank=True, null=True)
     country = models.CharField(max_length=256, blank=True, null=True)
     cost_of_new = models.FloatField(null=True, default=None)
@@ -9,12 +11,17 @@ class ComparativeApproach(CreatedMixin):
     par2_name = models.CharField(max_length=256, blank=True, null=True)
     par2_val = models.FloatField(null=True, default=None)
 
-class CostApproach(CreatedMixin):
+    class Meta:
+        db_table = 'comparative_approach'
+
+class CostApproach(CreateMixin):
     year = models.DateField(null=True, default=None)
     mileage = models.FloatField(null=True, default=None)
-    offer price = models.FloatField(null=True, default=None)
+    offer_price = models.FloatField(null=True, default=None)
     par1_name = models.CharField(max_length=256, blank=True, null=True)
     par1_val = models.FloatField(null=True, default=None)
     par2_name = models.CharField(max_length=256, blank=True, null=True)
     par2_val = models.FloatField(null=True, default=None)
 
+    class Meta:
+        db_table = "cost_approach"
